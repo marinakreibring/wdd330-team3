cc89552e7e56155840968c5789ab2295792cd96e
santichapa <santi.chaparro280@gmail.com>
2024-11-14 17:49:44 -0300

    individual activity chekpoint

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,12 @@
+class ProductListing {
+    constructor(category, dataSource, listElement) {
+        this.category = category
+        this.dataSource = dataSource
+        this.listElement = listElement
+    }
+    async init() {
+            const productList = await this.dataSource.getData()
+    }
+}
+
+export default ProductListing;

52b78880f83a9cd4b4ae2396a3238d0829bd2ee9
sarah-fox5443 <126546277+sarah-fox5443@users.noreply.github.com>
2024-11-16 21:35:13 -0800

    Remove items from list

--- src/js/cart.js
+++ src/js/cart.js
@@ -21,6 +21,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button class="danger">Remove</button>
 </li>`;
 
   return newItem;

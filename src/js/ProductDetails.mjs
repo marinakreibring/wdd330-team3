ff08f3d1e2b741f8088911633083bc8a96447feb
santichapa <santi.chaparro280@gmail.com>
2024-11-19 17:20:46 -0300

    corrected functions

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,5 +1,23 @@
 import ProductData from "./ProductData.mjs";
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+
+function productDetailsTemplate(product) {
+  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${product.Image}"
+      alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div></section>`;
+}
 
 export default class ProductDetails {
     constructor(productId, dataSource) {
@@ -11,7 +29,7 @@ export default class ProductDetails {
         // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
         this.product = await this.dataSource.findProductById(this.productId)
         // once we have the product details we can render out the HTML
-        this.renderProductDetails()
+        this.renderProductDetails("main")
         // once the HTML is rendered we can add a listener to Add to Cart button
         // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
         document.getElementById('addToCart')
@@ -19,38 +37,17 @@ export default class ProductDetails {
     }
 
     addProductToCart() {
-
         cart = getLocalStorage("so-cart") ?? [];
         cart.push(this.product);
         setLocalStorage("so-cart", cart);
     }
 
-    renderProductDetails() {
-        const p = this.product
-        const detailsHTML = `
-        <section class="product-detail">
-        <h3>${p.Brand.Name}</h3>
-
-        <h2 class="divider">${p.NameWithoutBrand}</h2>
-
-        <img
-          class="divider"
-          src="${p.Image}"
-          alt="${p.NameWithoutBrand}"
-        />
-
-        <p class="product-card__price">${p.FinalPrice}</p>
-
-        <p class="product__color">${p.Colors.ColorName}</p>
-
-        <p class="product__description">${p.DescriptionHtmlSimple}</p>
-
-        <div class="product-detail__add">
-          <button id="addToCart" data-id="${p.Id}">Add to Cart</button>
-        </div>
-      </section>`
-
-      document.querySelector("#details").innerHTML = detailsHTML
+    renderProductDetails(selector) {
+      const element = document.querySelector(selector);
+      element.insertAdjacentHTML(
+        "afterBegin",
+        productDetailsTemplate(this.product)
+      );
     }
   }
   
